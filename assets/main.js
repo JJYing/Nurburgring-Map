f92a6e2d5f984385ee1ca99f915f0345f0537ecc
JJ Ying <yingjunjiu@gmail.com>
2024-09-25 02:29:52 +0800

    Updated corner info & debug content & controls

--- assets/main.js
+++ assets/main.js
@@ -7,6 +7,10 @@ var d = new Vue({
     p: 0,
     w: 660,
     h: 530,
+    mX: 0,
+    mY: 0,
+    showAllCornerNames: true,
+    nightMode: false,
     showCorner: false,
     showSection: false,
     currentCorner: null,
@@ -18,7 +22,7 @@ var d = new Vue({
     sections: [
       {
         "st":.019,
-        "ed":.057,
+        "ed":.064,
         "x":.34,
         "y":.88,
         "ch": "Hatzenbach",
@@ -30,17 +34,21 @@ var d = new Vue({
       {
         "st": .006,
         "ed": .014,
-        "x": .34,
+        "x": .357,
         "y": .88,
-        "ch": "萨宾娜弯",
+        "h": "c",
+        "v": "b",
+        "ch": "萨宾娜",
         "en": "Sabine Schmitz Curve",
         "de": "Sabine Schmitz Kurve",
       },
       {
         "st": .019,
         "ed": .032,
-        "x": .28,
-        "y": .94,
+        "x": .32,
+        "y": .935,
+        "h": "c",
+        "v": "t",
         "ch": "Hatzenbach Bogen",
         "en": "Hunt Stream Curve",
         "de": "Hatzenbach Bogen",
@@ -50,17 +58,56 @@ var d = new Vue({
         "st": .038,
         "ed": .049,
         "x": .27,
-        "y": .82,
+        "y": .895,
+        "h": "l",
+        "v": "b",
+        "ch": "扭扭弯",
         "nk": "扭扭弯"
       },
       {
-        "st": .07,
-        "ed": .09,
-        "x": .27,
-        "y": .82,
-        "ch": "lalala",
-        "en": 69,
+        "st": .065,
+        "ed": .076,
+        "x": .224,
+        "y": .852,
+        "h": "l",
+        "v": "b",
+        "ch": "大橡树",
+        "de": "Hoheichen",
+        "en": "High Oaks",
+        "nk": "龙哥不杀弯"
+      },
+      {
+        "st": .077,
+        "ed": .089,
+        "x": .176,
+        "y": .838,
+        "h": "r",
+        "v": "t",
+        "ch": "小桥直线",
+        "nk": "小桥直线"
+      },
+      {
+        "st": .1,
+        "ed": .117,
+        "x": .156,
+        "y": .759,
+        "h": "l",
+        "v": "m",
+        "ch": "飞机场",
+        "de": "Flugplatz",
+        "more": "虽然很多赛车在这个高速弯起飞，但事实上这里叫「飞机场」的原因是在这个弯道边上以前是个飞机场",
       },      
+      {
+        "st": .179,
+        "ed": .188,
+        "x": .106,
+        "y": .526,
+        "h": "l",
+        "v": "m",
+        "ch": "Aremberg",
+        "de": "Aremberg",
+        "en": "Arem mountain",
+      }, 
     ]
   },
   methods: {
@@ -121,3 +168,16 @@ window.addEventListener('resize', function(){
 
 updateScrollDistance()
 updatePageHeight()
+
+document.querySelector('.track-map > .inner').addEventListener('mousemove', function(event) {
+  // 获取 .inner 元素的边界框
+  const innerRect = this.getBoundingClientRect();
+
+  // 计算鼠标在 .inner 内的位置
+  const mouseX = event.clientX - innerRect.left;
+  const mouseY = event.clientY - innerRect.top;
+
+  // 计算鼠标位置的比例
+  d.mX = mouseX / innerRect.width;
+  d.mY = mouseY / innerRect.height;
+});
