272929b3a6db3926899bca5b6cbe74b747fa8114
JJ Ying <yingjunjiu@gmail.com>
2024-10-11 11:04:52 +0800

    Optimized for Safari

--- index.php
+++ index.php
@@ -127,8 +127,8 @@ echo
           <span class="extra" title="德文" v-if="currentCorner.de">
             <svg viewBox="0 0 5 3" class="skew-p">
               <rect id="black_stripe" width="5" height="3" y="0" x="0" fill="#000"/>
-              <rect id="red_stripe" width="5" height="2" y="1" x="0" fill="#D00"/>
-              <rect id="gold_stripe" width="5" height="1" y="2" x="0" fill="#FFCE00"/>
+              <rect id="red_stripe" width="5" height="2" y="1" x="0" fill="#6C6C6C"/>
+              <rect id="gold_stripe" width="5" height="1" y="2" x="0" fill="#DADADA"/>
             </svg> {{currentCorner.de}}
           </span>
           <span class="extra"  title="英文" v-if="currentCorner.en">
@@ -140,11 +140,11 @@ echo
                 <path d="M30,15 h30 v15 z v15 h-30 z h-30 v-15 z v-15 h30 z"/>
               </clipPath>
               <g clip-path="url(#s)">
-                <path d="M0,0 v30 h60 v-30 z" fill="#012169"/>
+                <path d="M0,0 v30 h60 v-30 z" fill="#292929"/>
                 <path d="M0,0 L60,30 M60,0 L0,30" stroke="#fff" stroke-width="6"/>
-                <path d="M0,0 L60,30 M60,0 L0,30" clip-path="url(#t)" stroke="#C8102E" stroke-width="4"/>
+                <path d="M0,0 L60,30 M60,0 L0,30" clip-path="url(#t)" stroke="#646464" stroke-width="4"/>
                 <path d="M30,0 v30 M0,15 h60" stroke="#fff" stroke-width="10"/>
-                <path d="M30,0 v30 M0,15 h60" stroke="#C8102E" stroke-width="6"/>
+                <path d="M30,0 v30 M0,15 h60" stroke="#646464" stroke-width="6"/>
               </g>
               </svg> {{currentCorner.en}}
             </span>
