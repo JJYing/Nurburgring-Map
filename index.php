9ef9c205dd6fbcc7dabf5ba52f62fe0a5cf7efcb
JJ Ying <yingjunjiu@gmail.com>
2024-10-29 11:40:01 +0800

    Updated analytics

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ echo
       (function() {
         var u="//anyway.fm/matomo/";
         _paq.push(['setTrackerUrl', u+'matomo.php']);
-        _paq.push(['setSiteId', '2']);
+        _paq.push(['setSiteId', '3']);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
         g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
       })();
